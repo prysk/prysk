6f47d202ce8020ba94dac265339b17d21975eb15
Nicola Coretti <nico.coretti@gmail.com>
2022-06-26 14:12:24 +0200

    Add type checks for scripts

--- scripts/md5.py
+++ scripts/md5.py
@@ -1,24 +1,24 @@
 """ Cli tool to calculate the md5 checksum for one or multiple files """
-import argparse
 import sys
+from argparse import ArgumentParser, FileType
 from hashlib import md5
+from typing import Optional, Sequence
 
 
-def _create_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "files", metavar="FILE", type=argparse.FileType("rb"), nargs="+"
-    )
+def _create_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+    parser.add_argument("files", metavar="FILE", type=FileType("rb"), nargs="+")
     return parser
 
 
-def main(argv=None):
+def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = _create_parser()
     args = parser.parse_args(argv)
     for file in args.files:
         hasher = md5()
         hasher.update(file.read())
         print(hasher.hexdigest())
+    return 0
 
 
 if __name__ == "__main__":
