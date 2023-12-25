5a6e7604bd28af199899ff6fe9b111f60eb0f0ea
Tomas Janousek <tomi@nomi.cz>
2023-12-26 00:21:22 +0100

    Clean up findtests tests a bit
    Use Path.write_text instead of touch/open/write.
    Use tmp_path instead of the legacy tmpdir.

--- test/unit/test_test.py
+++ test/unit/test_test.py
@@ -14,28 +14,26 @@ def create_directory(root, name):
 
 def create_file(directory, name, data):
     file = Path(directory) / name
-    file.touch()
-    with open(file, "w") as f:
-        f.write(data)
+    file.write_text(data)
     return file
 
 
-def test_findtests_ignores_hidden_file_in_current_directory(tmpdir):
-    file = create_file(tmpdir, "default.t", "")
+def test_findtests_ignores_hidden_file_in_current_directory(tmp_path):
+    file = create_file(tmp_path, "default.t", "")
     expected = (Path(file.name),)
-    with cwd(tmpdir):
+    with cwd(tmp_path):
         assert tuple(_findtests([file.name])) == expected
 
 
-def test_findtests_ignores_hidden_files(tmpdir):
-    _ = create_file(tmpdir, ".hidden.t", "")
-    visible_file = create_file(tmpdir, "visible.t", "")
+def test_findtests_ignores_hidden_files(tmp_path):
+    _ = create_file(tmp_path, ".hidden.t", "")
+    visible_file = create_file(tmp_path, "visible.t", "")
     expected = (visible_file,)
-    assert tuple(_findtests([tmpdir])) == expected
+    assert tuple(_findtests([tmp_path])) == expected
 
 
-def test_findtests_ignores_folders(tmpdir):
-    hidden_directory = create_directory(tmpdir, ".hidden")
+def test_findtests_ignores_folders(tmp_path):
+    hidden_directory = create_directory(tmp_path, ".hidden")
     _ = create_file(hidden_directory, "visible.t", "")
     expected = tuple()
-    assert tuple(_findtests([tmpdir])) == expected
+    assert tuple(_findtests([tmp_path])) == expected
