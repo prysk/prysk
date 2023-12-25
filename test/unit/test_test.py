cef31e00d8fe7d175d4e3b42af15fca645115117
Tomas Janousek <tomi@nomi.cz>
2023-12-26 00:52:51 +0100

    Fix prysk test file lookup for explicitly specified hidden files/directories
    This restores the original cram behaviour of not ignoring files without
    a .t suffix, hidden files or hidden directories when these are
    explicitly specified on the command line.
    
    (is_hidden and is_testfile can be moved back into _findtests as the pytest
    plugin went away; is_hidden no longer needs to account for relative
    paths as Path.relative_to drops all the irrelevant parts)
    
    Fixes: https://github.com/prysk/prysk/issues/224

--- test/unit/test_test.py
+++ test/unit/test_test.py
@@ -37,3 +37,19 @@ def test_findtests_ignores_folders(tmp_path):
     _ = create_file(hidden_directory, "visible.t", "")
     expected = tuple()
     assert tuple(_findtests([tmp_path])) == expected
+
+
+def test_findtests_accepts_explicit_files(tmp_path):
+    file1 = create_file(tmp_path, "default.md", "")
+    file2 = create_file(tmp_path, ".hidden.t", "")
+    expected = (file1, file2)
+    assert tuple(_findtests([file1, file2])) == expected
+
+
+def test_findtests_accepts_explicit_dirs(tmp_path):
+    hidden_directory = create_directory(tmp_path, ".hidden")
+    hidden_subdir = create_directory(hidden_directory, ".hidden_subdir")
+    file = create_file(hidden_directory, "visible.t", "")
+    _ = create_file(hidden_subdir, "sub_visible.t", "")
+    expected = (file,)
+    assert tuple(_findtests([hidden_directory])) == expected
